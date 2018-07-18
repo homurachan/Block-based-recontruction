e964c69572669e5007b262d2e501a55c10031413
homurachan <homurachan@users.noreply.github.com>
2018-07-18 17:41:35 +0900

    Add files via upload

--- remove_misc_in_lst-center.py
+++ remove_misc_in_lst-center.py
@@ -7,25 +7,41 @@ except:
 	from optik import OptionParser
 
 def main():
-	(lstfile, image_name,wr_lst,center,write_LST) =  parse_command_line()
+	(lstfile, image_name,wr_lst,center,apix,cs,voltage,write_LST) =  parse_command_line()
 	g = open(lstfile, "r")
 	w=open(wr_lst,"w")
 	if(write_LST):
 		w.write("#LST\n")
 	lst_line = g.readlines()
 	temp=""
-
-	for i in range(1, len(lst_line)):
+	test=str(lst_line[0].split()[0])
+	center_pos=-1
+	nowrite=0
+	start=3
+	if(test=="#LST"):
+		start=1
+	len_len=int(len(lst_line[start].split()))
+	for j in range (2,len_len):
+		tmp=str((lst_line[start].split()[j]).split('=')[0])
+		if(tmp=="center"):
+			center_pos=j
+		if(tmp=="ampcont"):
+			nowrite=1
+	for i in range(start, len(lst_line)):
 #	for i in range(3, 5):
-		
 		temp=str(i-1)+"\t"+str(image_name)+"\t"
-		for k in range(2,8):
-			temp=temp+str(lst_line[i].split()[k])+"\t"
-	#	temp=temp+str(lst_line[i].split()[9])+"\t"
-		temp=temp+"center="+str(center)+","+str(center)+"\t"
-		for l in range(9,11):
-			temp=temp+str(lst_line[i].split()[l])+"\t"
-		temp=temp+str(lst_line[i].split()[11])+"\n"
+		for k in range(2,len(lst_line[i].split())):
+			if (k==center_pos):
+				temp=temp+"center="+str(center)+","+str(center)
+			else:
+				temp=temp+str(lst_line[i].split()[k])
+			if(k!=len(lst_line[i].split())-1):
+				temp=temp+"\t"
+			else:
+				if(nowrite):
+					temp=temp+"\n"
+				else:
+					temp=temp+"\tapix="+str(apix)+"\tcs="+str(cs)+"\tvoltage="+str(voltage)+"\tampcont=0.1\n"
 		w.write(temp)
 		temp=""
 		
@@ -34,11 +50,11 @@ def main():
 	
 	
 def parse_command_line():
-	usage="%prog <lstfile> <image filename> <changed lst> <center> <1 for write LST>"
+	usage="%prog <lstfile> <image filename> <changed lst> <center> <apix> <Cs> <voltage> <1 for write LST>"
 	parser = OptionParser(usage=usage, version="%1")
 	
-	if len(sys.argv)<4: 
-		print "<lstfile> <image filename> <changed lst> <center> <1 for write LST>"
+	if len(sys.argv)<9: 
+		print "<lstfile> <image filename> <changed lst> <center> <apix> <Cs> <voltage> <1 for write LST>"
 		sys.exit(-1)
 	
 	(options, args)=parser.parse_args()
@@ -47,8 +63,11 @@ def parse_command_line():
 	image_name=args[1]
 	wr_lst = args[2]
 	center=int(args[3])
-	write_LST = int(args[4])
-	return (lstfile, image_name,wr_lst,center,write_LST)
+	apix=float(args[4])
+	cs=float(args[5])
+	voltage=float(args[6])
+	write_LST = int(args[7])
+	return (lstfile, image_name,wr_lst,center,apix,cs,voltage,write_LST)
 
 if __name__== "__main__":
 	main()
