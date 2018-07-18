7fe080c277f32bd5abee4510e4fde36aab70c08b
homurachan <homurachan@users.noreply.github.com>
2018-07-18 14:56:00 +0900

    Add files via upload

--- relion2lst_even_odd.py
+++ relion2lst_even_odd.py
@@ -5,12 +5,9 @@ try:
 	from optparse import OptionParser
 except:
 	from optik import OptionParser
-#	You need a Refine3D data star file.
-#	Check the metadata, and find like "rlnRandomSubse #26", Replace "24" below into the 26-1
-#	Replace "33" below into x+4 in the final line of "rln****** #x"
 
 def main():
-	(starfile, lstfile, new_odd_lstfile, new_even_lstfile) =  parse_command_line()
+	(starfile,mline, lstfile, new_odd_lstfile, new_even_lstfile) =  parse_command_line()
 	f = open(starfile, "r")
 	g = open(lstfile, "r")
 	h = open(new_odd_lstfile, "w")
@@ -20,13 +17,25 @@ def main():
 	j=0
 	h.write("#LST\n")
 	l.write("#LST\n")
-	for i in range(33, len(star_line)):
-		DATA_line = int(star_line[i].split()[24])
-		if DATA_line == 1:
-			h.write(str(lst_line[j+1]))
-		else:
-			l.write(str(lst_line[j+1]))
-		j+=1
+	found=0
+	for i in range(4,mline):
+		metadata_name=str(star_line[i].split()[0])
+#		print metadata_name
+		if(metadata_name=="_rlnRandomSubset"):
+			found=1
+			MN=int(star_line[i].split("#")[1])
+	if (found==0):
+		print ("The star file dose not contains random subset imformation. EXIT.")
+		sys.exit(-1)
+	for i in range(mline, len(star_line)):
+		if(star_line[i].split()):
+			DATA_line = int(star_line[i].split()[MN-1])
+			if DATA_line == 1:
+				h.write(str(lst_line[j+1]))
+			else:
+				l.write(str(lst_line[j+1]))
+			j+=1
+			
 	h.close()
 	f.close()
 	g.close()
@@ -34,22 +43,23 @@ def main():
 	
 	
 def parse_command_line():
-	usage="%prog <starfile> <lstfile> <new odd lstfile> <new even lstfile>"
+	usage="%prog <starfile> <metadata line +4> <lstfile> <new odd lstfile> <new even lstfile>"
 	parser = OptionParser(usage=usage, version="%1")
 	
-	if len(sys.argv)<4: 
-		print "<starfile> <lstfile> <new odd lstfile> <new even lstfile>"
+	if len(sys.argv)<6: 
+		print "<starfile> <metadata line +4> <lstfile> <new odd lstfile> <new even lstfile>"
 		sys.exit(-1)
 	
 	(options, args)=parser.parse_args()
 
 
 	starfile = args[0]
-	lstfile = args[1]
-	new_odd_lstfile = args[2]
-	new_even_lstfile = args[3]
+	mline=int(args[1])
+	lstfile = args[2]
+	new_odd_lstfile = args[3]
+	new_even_lstfile = args[4]
 	
-	return (starfile, lstfile, new_odd_lstfile, new_even_lstfile)
+	return (starfile,mline, lstfile, new_odd_lstfile, new_even_lstfile)
 
 
 
