280ed244ef04fdde673c0ac0206d601ef959c1be
homurachan <homurachan@users.noreply.github.com>
2022-04-22 20:42:13 +0900

    Update BBR_with_relion_v9_relion31.py

--- for_straightforward_relion_v2/BBR_with_relion_v9_relion31.py
+++ for_straightforward_relion_v2/BBR_with_relion_v9_relion31.py
@@ -318,8 +318,10 @@ def determin_symmetry(SYMMETRY):
 		sym_num=2*int(SYMMETRY[1:])
 	if(a=="O"):
 		sym_num=24
-	if(a=="I" and b==3):
-		sym_num=60
+	if(a=="I"):
+		b=int(SYMMETRY[1:])
+		if(b==3):
+			sym_num=60
 
 	return (a,sym_num)
 OCT=[]
